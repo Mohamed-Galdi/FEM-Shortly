3e300bc286bba17877b4f376fc3ae91669aea5bb
M-Galdi <GALDI.MOHAMED@ofppt-edu.ma>
2023-08-03 10:52:11 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <p className="text-7xl">
+        hello from vs code
+      </p>
+    </>
+  )
+}
+
+export default App
